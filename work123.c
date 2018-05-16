ffedaee2ef0dbecfe488647a7bcb168e0539d060
jeff1231 <asdjeff1231@gmail.com>
2018-05-17 01:15:47 +0800

    wwwww

--- work123.c
+++ work123.c
@@ -4,10 +4,10 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int GitWork;
-	for(GitWork=1;GitWork<11;GitWork++)
+	int GitWork; 
+	for(GitWork=1;GitWork<11;GitWork++) //for迴圈做10次 
 	{
-		printf("%d ",GitWork);
+		printf("%d ",GitWork);//每做一次 印出目前值 
 	}
 	
 	
