f8d35b4c3521fd9f81935aad8974cb8636dee773
jeff1231 <asdjeff1231@gmail.com>
2018-05-17 00:36:34 +0800

    wwwww

---
+++ work123.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	int i;
+	for(i=1;i<11;i++)
+	{
+		printf("%d ",i);
+	}
+	
+	
+	return 0;
+}
